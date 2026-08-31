1ebb362552a929ef1a7b282044df59b26772ab12
Sri Charan Uppuluri <sricharanuppuluri1739@gmail.com>
2026-08-31 11:30:31 +0530

    0011-container-with-most-water Medium .java Array 08-31-2026

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max_area = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+            if (area > max_area) {
+                max_area = area;
+            }
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max_area;
+    }
+}
